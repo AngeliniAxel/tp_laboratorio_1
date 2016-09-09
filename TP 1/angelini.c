a4083bc61502dc7adf131a8e48b483f83a0847b5
AngeliniAxel <axel.angelini@gmail.com>
2016-09-08 23:59:48 -0300

    TP 1 Terminado

--- TP 1/angelini.c
+++ TP 1/angelini.c
@@ -1,44 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+    //brief recibe un float y lo retorna
     float pedirOperando(void)
     {
         float x;
-        printf("Ingrese el numero: ");
+        printf("\nIngrese el numero: ");
         fflush(stdin);
         scanf("%f",&x);
         return x;
     }
 
+    //brief realiza la suma de dos numeros
+    //variable x toma el primer numero
+    //variable y toma el segundo numero
+    //return devuelve el resultado de la suma
     float funcionSuma(float x,float y)
     {
         return x + y;
     }
 
+    //brief realiza la resta de dos numeros
+    //variable x toma el primer numero
+    //variable y toma el numero a restar
+    //return devuelve el resultado de la resta
     float funcionResta(float x, float y)
     {
         return x - y;
     }
 
-
+    //brief realiza la division entre dos numeros
+    //variable x toma el dividendo
+    //variable y toma el divisor
+    //return devuelve el resultado de la division
     float funcionDivision(float x, float y)
     {
-        if(y==0)
-        {
-            printf("No se puede dividir por 0\n\n");
-            system("pause");
-        }else
-        {
             return x / y;
-        }
     }
 
+    //brief realiza la multiplicacion entre dos numeros
+    //variable x toma el primer numero
+    //variable y toma el segundo numero
+    //return devuelve el resultado de la multiplicacion
     float funcionMultiplicacion(float x, float y)
     {
         return x * y;
     }
 
+    //brief realiza el factorial de un numero
+    //variable x toma el numero a factorear
+    //return devuelve el resultado
     float funcionFactorial(float x)
     {
         int i;
--- TP 1/main.c
+++ TP 1/main.c
@@ -23,10 +23,19 @@ int main()
         printf("5- Calcular la division (A/B)\n");
         printf("6- Calcular la multiplicacion (A*B)\n");
         printf("7- Calcular el factorial (A!)\n");
-        printf("8- Calcular todas las operacione\n");
+        printf("8- Calcular todas las operaciones\n");
         printf("9- Salir\n");
+        printf("\nIngrese opcion: ");
 
         scanf("%d",&opcion);
+
+        // Toma el dato y valida que este dentro del rango de opciones disponibles
+        while(opcion < 1 || opcion > 9)
+        {
+            printf("\nError. Ingrese una opcion valida: ");
+            fflush(stdin);
+            scanf("%d", &opcion);
+        }
 
         switch(opcion)
         {
@@ -43,54 +52,68 @@ int main()
             case 3:
 
                 suma = funcionSuma(a,b);
-                printf("El resultado de la suma es: %.2f\n\n",suma);
+                printf("\nEl resultado de la suma es: %.2f\n\n",suma);
                 system("pause");
                 break;
 
             case 4:
 
                 resta = funcionResta(a,b);
-                printf("El resultado de la resta es: %2.f\n\n",resta);
+                printf("\nEl resultado de la resta es: %2.f\n\n",resta);
                 system("pause");
                 break;
 
             case 5:
 
-                division = funcionDivision(a,b);
-                printf("El resultado de la division es: %.2f\n\n",division);
-                system("pause");
+                //Valida que el divisor sea mayor a 0
+                if(b==0)
+                {
+                    printf("\nNo se puede dividir por 0\n\n");
+                    system("pause");
+                }else
+                {
+                    division = funcionDivision(a,b);
+                    printf("\nEl resultado de la division es: %.2f\n\n",division);
+                    system("pause");
+                }
                 break;
 
             case 6:
 
                 multiplicacion = funcionMultiplicacion(a,b);
-                printf("El resultado de la multiplicacion es: %.2f\n\n",multiplicacion);
+                printf("\nEl resultado de la multiplicacion es: %.2f\n\n",multiplicacion);
                 system("pause");
                 break;
 
             case 7:
 
                 factorial = funcionFactorial(a);
-                printf("El resultado del factorial es: %.2f\n\n",factorial);
+                printf("\nEl resultado del factorial es: %.2f\n\n",factorial);
                 system("pause");
                 break;
 
             case 8:
 
                 suma = funcionSuma(a,b);
-                printf("El resultado de la suma es: %.2f\n\n",suma);
+                printf("\nEl resultado de la suma es: %.2f\n\n",suma);
 
                 resta = funcionResta(a,b);
                 printf("El resultado de la resta es: %2.f\n\n",resta);
 
-                division = funcionDivision(a,b);
-                printf("El resultado de la division es: %.2f\n\n",division);
+                if(b==0)
+                {
+                    printf("No se puede dividir por 0\n\n");
+                }else
+                {
+                    division = funcionDivision(a,b);
+                    printf("El resultado de la division es: %.2f\n\n",division);
+                }
 
                 multiplicacion = funcionMultiplicacion(a,b);
                 printf("El resultado de la multiplicacion es: %.2f\n\n",multiplicacion);
 
                 factorial = funcionFactorial(a);
-                printf("El resultado del factorial es: %.2f\n\n",factorial);
+                printf("El resultado del factorial es: %.2f\n\n\n\n",factorial);
 
                 system("pause");
 
@@ -100,6 +123,9 @@ int main()
         }
 
     }while(opcion != 9);
+
+    system("cls");
+    printf("\nHASTA LUEGO!!!\n\n");
 
 
 
